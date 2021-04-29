41014dee808b15e073fe1930c4c51a8a333133dc
Neeraj Amoli <neeraj.amoli200@gmail.com>
2021-04-29 11:10:43 +0530

    user migration added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, param: :_username
+      post '/auth/login', to: 'authentication#login'
+      get '/*a', to: 'application#not_found'
+    end
+  end
 end
